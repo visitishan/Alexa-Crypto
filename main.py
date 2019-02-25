8da0941db1890141dc46323b56ccbd35761cc0c4
Ishan Jain <visitishan@gmail.com>
2019-02-26 02:37:54 +0530

    Main file to be hosted on AWS Lambda

---
+++ main.py
@@ -0,0 +1,114 @@
+import json
+import requests
+
+r = requests.get('https://koinex.in/api/ticker')
+jsonfile = r.json()
+Price = jsonfile['prices']['inr']['BTC']
+
+def lambda_handler(event, context):
+    if event['session']['new']:
+        on_start()
+    if event['request']['type'] == "LaunchRequest":
+        return on_launch(event)
+    elif event['request']['type'] == "IntentRequest":
+        return intent_scheme(event)
+    elif event['request']['type'] == "SessionEndedRequest":
+        return on_end()
+
+#------------------------------Part3--------------------------------
+# Here we define the Request handler functions
+def on_start():
+    print("Session Started.")
+
+def on_launch(event):
+    onlunch_MSG = "Hi, Welcome to the Crypto Currency Price Alexa Skill. You can check the current price of Bitcoin in Indian Rupee  with it. You could say for example: price of bitcoin"
+    reprompt_MSG = "Do you want to know the current price of Bitcoin?"
+    card_TEXT = "Ask me about Bitcoin price."
+    card_TITLE = "Ask me about Bitcoin price."
+    return output_json_builder_with_reprompt_and_card(onlunch_MSG, card_TEXT, card_TITLE, reprompt_MSG, False)
+
+def on_end():
+    print("Session Ended.")
+#-----------------------------Part3.1-------------------------------
+# The intent_scheme(event) function handles the Intent Request. 
+# Since we have a few different intents in our skill, we need to 
+# configure what this function will do upon receiving a particular 
+# intent. This can be done by introducing the functions which handle 
+# each of the intents.
+def intent_scheme(event):
+    
+    intent_name = event['request']['intent']['name']
+
+    if intent_name == "cryptocurrency":
+        return player_bio(event)        
+    elif intent_name in ["AMAZON.StopIntent", "AMAZON.CancelIntent"]:
+        return stop_the_skill(event)
+    elif intent_name == "AMAZON.HelpIntent":
+        return assistance(event)
+    elif intent_name == "AMAZON.NavigateHomeIntent":
+        return fallback_call(event)
+#---------------------------Part3.1.1-------------------------------
+# Here we define the intent handler functions
+def player_bio(event):
+    reprompt_MSG = "Do you want to know the price of Bitcoin?"
+    card_TEXT = "Price of Bitcoin is rupees " + Price
+    card_TITLE = "Price of Bitcoin is rupees " + Price
+    return output_json_builder_with_reprompt_and_card(card_TEXT, card_TEXT, card_TITLE, reprompt_MSG, False)
+
+def stop_the_skill(event):
+    stop_MSG = "Thank you. Bye!"
+    reprompt_MSG = ""
+    card_TEXT = "Bye."
+    card_TITLE = "Bye Bye."
+    return output_json_builder_with_reprompt_and_card(stop_MSG, card_TEXT, card_TITLE, reprompt_MSG, True)
+    
+def assistance(event):
+    assistance_MSG = "You can ask me for the price of Bitcoin."
+    reprompt_MSG = "Do you want to know the price of Bitcoin?"
+    card_TEXT = "You've asked for help."
+    card_TITLE = "Help"
+    return output_json_builder_with_reprompt_and_card(assistance_MSG, card_TEXT, card_TITLE, reprompt_MSG, False)
+
+def fallback_call(event):
+    fallback_MSG = "I can't help you with that, try rephrasing the question or ask for help by saying HELP."
+    reprompt_MSG = "Do you want to know the price of Bitcoin?"
+    card_TEXT = "You've asked a wrong question."
+    card_TITLE = "Wrong question."
+    return output_json_builder_with_reprompt_and_card(fallback_MSG, card_TEXT, card_TITLE, reprompt_MSG, False)
+#------------------------------Part4--------------------------------
+# The response of our Lambda function should be in a json format. 
+# That is why in this part of the code we define the functions which 
+# will build the response in the requested format. These functions
+# are used by both the intent handlers and the request handlers to 
+# build the output.
+def plain_text_builder(text_body):
+    text_dict = {}
+    text_dict['type'] = 'PlainText'
+    text_dict['text'] = text_body
+    return text_dict
+
+def reprompt_builder(repr_text):
+    reprompt_dict = {}
+    reprompt_dict['outputSpeech'] = plain_text_builder(repr_text)
+    return reprompt_dict
+    
+def card_builder(c_text, c_title):
+    card_dict = {}
+    card_dict['type'] = "Simple"
+    card_dict['title'] = c_title
+    card_dict['content'] = c_text
+    return card_dict    
+
+def response_field_builder_with_reprompt_and_card(outputSpeach_text, card_text, card_title, reprompt_text, value):
+    speech_dict = {}
+    speech_dict['outputSpeech'] = plain_text_builder(outputSpeach_text)
+    speech_dict['card'] = card_builder(card_text, card_title)
+    speech_dict['reprompt'] = reprompt_builder(reprompt_text)
+    speech_dict['shouldEndSession'] = value
+    return speech_dict
+
+def output_json_builder_with_reprompt_and_card(outputSpeach_text, card_text, card_title, reprompt_text, value):
+    response_dict = {}
+    response_dict['version'] = '1.0'
+    response_dict['response'] = response_field_builder_with_reprompt_and_card(outputSpeach_text, card_text, card_title, reprompt_text, value)
+    return response_dict
